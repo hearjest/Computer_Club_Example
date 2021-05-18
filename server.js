62abbafa2ffe31fb35943874d5dafa9fb8d4bbce
hearjest <79370810+hearjest@users.noreply.github.com>
2021-05-18 16:39:10 -0400

    Added a server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require("http");
-/*const fs=require("fs");*/
-
+const fs=require("fs");
+const mime=require("mime-types");
 http.createServer(function(req,res){
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.write("<!DOCTYPE html><html><body><p>goodbye, world</p></body></html>");
-    res.end();
+    const path="."+(req.url||"/");
+    //fs.readFile("index.html",(err,data) => {
+        fs.readFile(path,(err,data) => {
+        if(err){
+            res.writeHead(404);
+            res.end();
+            return;
+        } else {
+        res.setHeader("Content-Type", mime.lookup(path));
+            res.writeHead(200);
+            res.write(data);
+            //res.write("<!DOCTYPE html><html><body><p>goodbye, world</p></body></html>");
+            
+            res.end();
+        }
+    });
+
 }).listen(8000); 
