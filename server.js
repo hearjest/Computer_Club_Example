1fa76cea7fea027f414eb4f2be9f2d5b17e95652
hearjest <79370810+hearjest@users.noreply.github.com>
2021-06-01 16:04:34 -0400

    Worked on the server stuff

--- server.js
+++ server.js
@@ -1,6 +1,45 @@
 const http = require("http");
 const fs=require("fs");
 const mime=require("mime-types");
+const express=require("express");
+
+const path=require("path");
+
+const app=express();
+app.get("*",(req,res) => {
+    let page="."+(req.url||"/");
+    if(page.endsWith("/")){
+        page=page+"index.html";
+    }
+  
+    //fs.readFile("index.html",(err,data) => {
+        fs.readFile(page,(err,data) => {
+        if(err){
+            res.status(404);
+            if(ext===".html"||ext===""){
+                fs.readFile("/404.html",(err,data)=>{
+                    if(err){
+                        res.status(500);
+                    }else{
+                        res.write(data);
+                    }
+                });
+            }
+            res.end();
+            return;
+        } else {
+            res.type(path.extname(page));
+       // res.setHeader("Content-Type", mime.lookup(path));
+            res.status(200);
+            res.write(data);
+            //res.write("<!DOCTYPE html><html><body><p>goodbye, world</p></body></html>");
+            
+            res.end();
+        }
+    });
+});
+app.listen(process.env.PORT||8000);
+/*
 http.createServer(function(req,res){
     const path="."+(req.url||"/");
     //fs.readFile("index.html",(err,data) => {
@@ -19,4 +58,5 @@ http.createServer(function(req,res){
         }
     });
 
-}).listen(8000); 
+}).listen(process.env.PORT||8000); 
+*/
