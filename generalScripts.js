bf51f0a6844a154c8746714410aaed1025033971
hearjest <79370810+hearjest@users.noreply.github.com>
2021-06-23 14:21:43 -0400

    Cleaned up darkmode code, cut some lines

--- generalScripts.js
+++ generalScripts.js
@@ -14,22 +14,9 @@ function openCloseDropDown(){
   };
 
  function lightLightModeToggle(){
-    document.body.classList.toggle("lightMode");
-    var toggleDropDowns =document.getElementsByClassName("dropDownButton");
-    var temp = document.getElementsByClassName("link");
-    for(var i=0;i<temp.length;i++){
-      if(temp[i].classList.contains("lightMode")){
-        temp[i].classList.remove("lightMode");
-      }else{
-        temp[i].classList.add("lightMode");
-      }
-    }
-
-    for(i=0;i<toggleDropDowns.length;i++){
-      if(toggleDropDowns[i].classList.contains("lightMode")){
-        toggleDropDowns[i].classList.remove("lightMode");
-      }else{
-        toggleDropDowns[i].classList.add("lightMode");
-      }
-    }
+   if(document.body.classList.contains("lightMode")){
+     document.body.classList.remove("lightMode");
+   }else{
+    document.body.classList.add("lightMode");
+   }
   }
